4c07ef88dd286d081c8097b4a1dd558c2821eab5
dhars-hub <dharshinib89@gmail.com>
2026-08-21 12:22:21 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,14 @@
 <?php
-$con=mysql_connect("localhost","root","");
-if (!$con)  {  
-die('Could not connect: ' . mysql_error()); 
- }
-$db=mysql_select_db("tollplazz");
+
+$host = "sql112.infinityfree.com";
+$username = "if0_42710828";
+$password = "YOUR_MYSQL_PASSWORD";
+$database = "if0_42710828_tollplazz";
+
+$con = mysqli_connect($host, $username, $password, $database);
+
+if (!$con) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
 
 ?>
